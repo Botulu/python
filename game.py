15cdf09be9328fb5ff4aaf4d2a525b18a841c32b
Botulu <80466714+Botulu@users.noreply.github.com>
2022-02-08 18:52:11 +0900

    Initial commit

---
+++ game.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((1280,720))
+
+pygame.display.set_caption("Cubes Game")
+#Параметры игрока
+x = 50
+y = 50
+width = 40
+height = 60
+speed = 5
+
+run=True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x-=speed
+    if keys[pygame.K_RIGHT]:
+        x+=speed
+    if keys[pygame.K_UP]:
+        y-=speed
+    if keys[pygame.K_DOWN]:
+        y+=speed
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (0,0,255),(x,y,width,height))
+    pygame.display.update()
+
+pygame.QUIT()
